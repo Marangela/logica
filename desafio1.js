6945dfeac84f704b854b64abba3c7c904c8ba449
Marangela <160649379+Marangela@users.noreply.github.com>
2024-03-15 17:23:26 +0000

    Desafio

---
+++ desafio1.js
@@ -0,0 +1,13 @@
+const nota1 = 7;
+const nota2 = 5;
+const nota3 = 4;
+
+const media = (nota1 + nota2 + nota3) / 3
+
+if(media >= 7){
+    console.log("Aprovado!")
+} else if(media <= 4){
+    console.log("Reprovado!")
+} else {
+    console.log("Aluno em recuperação")
+}
